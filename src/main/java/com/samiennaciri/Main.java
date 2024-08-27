092e8f5b454a9384b6cabf2e10e83f2b2fad746e
Ciri8 <ennacirisami1@gmail.com>
2024-08-27 12:54:56 -0400

    Connected customer model to Postman for get request

--- src/main/java/com/samiennaciri/Main.java
+++ src/main/java/com/samiennaciri/Main.java
@@ -1,7 +1,89 @@
 package com.samiennaciri;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; //first we have to say that this is from spring boot by importing
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Objects;
+//This code defines a Spring Boot application with a simple Customer class that can be used as a model for handling customer data (like ID, name, email, and age). This customer data could potentially be stored in a database later on, although in the current form, the code does not yet interact with a database.
+@SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        SpringApplication.run(Main.class, args);
     }
-}
+    class Customer{
+        private Integer id;
+        private String name;
+        private String email;
+        private String age;
+       public Customer(){};//public constructor
+        public Customer(Integer id, String name, String email, String age) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.age = age;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getAge() {
+            return age;
+        }
+
+        public void setAge(String age) {
+            this.age = age;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Customer customer = (Customer) o;
+            return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, email, age);
+        }
+
+        @Override
+        public String toString() {
+            return "Customer{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", email='" + email + '\'' +
+                    ", age='" + age + '\'' +
+                    '}';
+        }
+    }
+
+
+    }
+
+
+
